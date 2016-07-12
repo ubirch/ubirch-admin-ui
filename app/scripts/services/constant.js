31453e8bf78f02c6db996976abafe9acd8cd8d45
dieAte <beate.fiss@cameo.io>
2016-07-12 10:41:33 +0200

    new server-uri

--- app/scripts/services/constant.js
+++ app/scripts/services/constant.js
@@ -9,6 +9,6 @@
  */
 angular.module('ubirchAdminCrudApp')
   .constant('constant', {
-    REST_ENDPOINT_URL : "http://search-ubirch-device-data-3bfmzb4qqzvbj6cwxvhxwnol6y.us-east-1.es.amazonaws.com/",
+    REST_ENDPOINT_URL : "https://kibana.ci.ubirch.com/",
     UBIRCH_INDEX : "ubirch-device-data"
   });
