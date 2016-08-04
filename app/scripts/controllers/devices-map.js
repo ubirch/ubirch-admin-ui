15582e8468f8ada37c00dde38bbc304be870308b
dieAte <beate.fiss@cameo.io>
2016-08-04 18:57:21 +0200

    map with markers

--- app/scripts/controllers/devices-map.js
+++ app/scripts/controllers/devices-map.js
@@ -15,6 +15,21 @@ angular.module('ubirchAdminCrudApp')
         lng: 13.480031490325928,
         zoom: 12
       },
+      markers: {
+        ferzensMarker: {
+          lat: 52.50466320614026,
+          lng: 13.480031490325928,
+          message: "Ferzens live here!",
+          focus: true,
+          draggable: false
+        },
+        testMarker: {
+          lat: 52.48583796201143,
+          lng: 13.490266799926758,
+          message: "Testmarker",
+          draggable: false
+        }
+      },
       defaults: {
         scrollWheelZoom: false
       }
