40348b5bba44413c10045d791ee5928ae50a1470
dieAte <beate.fiss@cameo.io>
2016-11-07 11:47:29 +0100

    show device type icon in devices list

--- app/scripts/directives/devicetype.js
+++ app/scripts/directives/devicetype.js
@@ -7,35 +7,15 @@
  * # devicetype
  */
 angular.module('ubirchAdminCrudApp')
-  .directive('devicetype', function () {
+  .directive('devicetypeicon', [ 'DeviceTypes', function (DeviceTypes) {
     return {
       template: '<i class="sensor-type-icon {{iconStr}}"></i>',
       restrict: 'E',
       replace: true,
       scope: {deviceType: '@'
       },
-      link: function postLink(scope, element, attrs) {
-        scope.iconStr = "ion-radio-waves";
-
-        if( attrs.deviceType !== undefined) {
-
-          switch (attrs.deviceType) {
-            case "lightsSensor":
-              scope.iconStr = "ion-ios-lightbulb";
-              break;
-            case "temperaturesSensor":
-              scope.iconStr = "ion-thermometer";
-              break;
-            case "noisesSensor":
-              scope.iconStr = "ion-mic-c";
-              break;
-            case "wetnessSensor":
-              scope.iconStr = "ion-waterdrop";
-              break;
-            default:
-              scope.iconStr = "ion-radio-waves";
-          }
-        }
+      link: function postLink(scope) {
+        scope.iconStr = DeviceTypes.getDeviceTypeIcon(scope.deviceType);
       }
     };
-  });
+  }]);
