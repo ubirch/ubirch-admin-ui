daa57c44bca7e52e9e5cf6b08be3af178b5e219c
dieAte <beate.fiss@cameo.io>
2016-11-10 10:14:04 +0100

    sync state with boolean value syncState directive watches parameter now

--- app/scripts/directives/syncstate.js
+++ app/scripts/directives/syncstate.js
@@ -14,6 +14,26 @@ angular.module('ubirchAdminCrudApp')
       restrict: 'E',
       replace: true,
       scope: {syncState: '@'
+      },
+      link: function postLink(scope) {
+
+        var getSyncState = function(newSyncValue){
+          if (newSyncValue === 'true'){
+            scope.inSync = true;
+          }
+          else if (newSyncValue === 'false'){
+            scope.inSync = false;
+          }
+          else {
+            scope.inSync = undefined;
+          }
+        };
+
+        getSyncState(scope.syncState);
+
+        scope.$watch('syncState', function(newSyncValue){
+          getSyncState(newSyncValue);
+        });
       }
     };
   });
