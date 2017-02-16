d774c91dccebe1cfea188e9b71a9072e56a4e0cc
Falko Zurell <falko.zurell@ubirch.com>
2017-02-16 22:29:26 +0100

    first draft of build scripts for GoCD
    Signed-off-by: Falko Zurell <falko.zurell@ubirch.com>

--- buildWeb.sh
+++ buildWeb.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/env bash
-
+#!/usr/bin/env sh
+env
 echo build ubirch admin UI
-
+echo "create dist version for stage >>$1<<"
+echo install missing npm components
+npm install
+echo install missing bower components
+bower install --allow-root
 grunt build --staging=$1
