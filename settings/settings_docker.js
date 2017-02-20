cbf24b6d41e27e38ba0f9aab8f1994a598cfdab6
Falko Zurell <falko.zurell@ubirch.com>
2017-02-20 20:44:16 +0100

    trying to create a usable frontend with IP
    Signed-off-by: Falko Zurell <falko.zurell@ubirch.com>

--- settings/settings_docker.js
+++ settings/settings_docker.js
@@ -3,7 +3,7 @@
  */
 angular.module('ubirchAdminCrudApp')
   .constant('settings', {
-    UBIRCH_API_HOST : "avatar-service",
+    UBIRCH_API_HOST : "http://52.174.183.184",
     UBIRCH_AUTH_SERVICE_API_HOST : "${UBIRCH_AUTH_SERVICE_API_HOST}",
     DEFAULT_AUTH_EXPIRED_SECS: -1,
     DEFAULT_DEVICE_TYPE_KEY: "unknownDeviceType"
