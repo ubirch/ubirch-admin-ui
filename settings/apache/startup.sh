407ccae22115e6a51f8674db13b3e97c81cfefdb
Falko Zurell <falko.zurell@ubirch.com>
2017-03-09 21:17:51 +0100

    fixing replacement of ENV variable for Docker Container

--- settings/apache/startup.sh
+++ settings/apache/startup.sh
@@ -3,7 +3,7 @@
 
 if [ -d /var/www/html/scripts ]; then
   echo "Replacing variables UBIRCH_API_HOST in /var/www/html/scripts/scripts.*.js"
-  sed -i.bak "s%localhost:8080%${UBIRCH_API_HOST:-localhost:8080}%" /var/www/html/scripts/scripts.*.js
+  sed -i.bak "s%@@UBIRCH_API_HOST@@%${UBIRCH_API_HOST:-localhost:8080}%" /var/www/html/scripts/scripts.*.js
   if [ $? -ne 0 ]; then
     echo "Search string localhost:8080 not found in /var/www/html/scripts/scripts.*.js"
   fi
--- settings/settings_docker.js
+++ settings/settings_docker.js
@@ -3,7 +3,7 @@
  */
 angular.module('ubirchAdminCrudApp')
   .constant('settings', {
-    UBIRCH_API_HOST : "${UBIRCH_API_HOST}",
+    UBIRCH_API_HOST : "@@UBIRCH_API_HOST@@",
     UBIRCH_AUTH_SERVICE_API_HOST : "${UBIRCH_AUTH_SERVICE_API_HOST}",
     DEFAULT_AUTH_EXPIRED_SECS: -1,
     DEFAULT_DEVICE_TYPE_KEY: "unknownDeviceType"
