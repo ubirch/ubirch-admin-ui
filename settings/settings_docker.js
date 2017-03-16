9fdd3771d92309ca2660bdf6ac76047c36dff4b7
derMicha <dermicha@indarium.de>
2017-03-16 22:31:03 +0100

    fixed placeholder

--- settings/settings_docker.js
+++ settings/settings_docker.js
@@ -5,7 +5,7 @@ angular.module('ubirchAdminCrudApp')
   .constant('settings', {
     CONTEXT: "ubirch-admin-ui",
     UBIRCH_API_HOST : "@@UBIRCH_API_HOST@@",
-    UBIRCH_AUTH_SERVICE_API_HOST : "${UBIRCH_AUTH_SERVICE_API_HOST}",
+    UBIRCH_AUTH_SERVICE_API_HOST: "@@UBIRCH_AUTH_SERVICE_API_HOST@@",
     DEFAULT_AUTH_EXPIRED_SECS: -1,
     DEFAULT_DEVICE_TYPE_KEY: "unknownDeviceType"
   });
