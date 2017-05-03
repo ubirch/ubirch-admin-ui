2a8a6233ee9315b53ad32cd821bede8983ba8718
Moellus <matthias.moeller@ubirch.com>
2017-05-03 12:58:53 +0200

    replace UBIRCH_AUTH_SERVICE_API_HOST and UBIRCH_USER_SERVICE_API_HOST vars

--- settings/apache/startup.sh
+++ settings/apache/startup.sh
@@ -3,11 +3,20 @@
 env 
 
 if [ -d /var/www/html/scripts ]; then
+
   echo "Replacing variables UBIRCH_API_HOST in /var/www/html/scripts/scripts.*.js"
   sed -i.bak "s%@@UBIRCH_API_HOST@@%${UBIRCH_API_HOST:-localhost:8080}%" /var/www/html/scripts/scripts.*.js
+
+  echo "Replacing variables UBIRCH_AUTH_SERVICE_API_HOST in /var/www/html/scripts/scripts.*.js"
+  sed -i.bak "s%@@UBIRCH_AUTH_SERVICE_API_HOST@@%${UBIRCH_AUTH_SERVICE_API_HOST:-localhost:8080}%" /var/www/html/scripts/scripts.*.js
+
+  echo "Replacing variables UBIRCH_USER_SERVICE_API_HOST in /var/www/html/scripts/scripts.*.js"
+  sed -i.bak "s%@@UBIRCH_USER_SERVICE_API_HOST@@%${UBIRCH_USER_SERVICE_API_HOST:-localhost:8080}%" /var/www/html/scripts/scripts.*.js
+
   if [ $? -ne 0 ]; then
     echo "Search string localhost:8080 not found in /var/www/html/scripts/scripts.*.js"
   fi
+
 fi
 
 /usr/sbin/apache2ctl -D FOREGROUND
