766394528360602ef65d458eb19bf0af2922d5be
dieAte <beate.fiss@cameo.io>
2017-05-09 11:07:11 +0200

    Version for ferdi: admin-ui running on localhost accesses dev services

--- settings/settings_demo.js
+++ settings/settings_demo.js
@@ -4,9 +4,9 @@
 angular.module('ubirchAdminCrudApp')
   .constant('settings', {
     CONTEXT: "ubirch-admin-ui-demo",
-    UBIRCH_API_HOST : "http://ubirch.dev.ubirch.com:80",
+    UBIRCH_API_HOST : "http://ubirch.demo.ubirch.com:80",
     DEFAULT_DEVICE_TYPE_KEY: "unknownDeviceType",
-    UBIRCH_AUTH_SERVICE_API_HOST : "http://auth.dev.ubirch.com:8091",
-    UBIRCH_USER_SERVICE_API_HOST : "http://auth.dev.ubirch.com:8091",
+    UBIRCH_AUTH_SERVICE_API_HOST : "http://auth.demo.ubirch.com:8091",
+    UBIRCH_USER_SERVICE_API_HOST : "http://auth.demo.ubirch.com:8091",
     DEFAULT_AUTH_EXPIRED_SECS: -1   // if OP didn't deliver expires_in parameter this is the default number of seconds after which authentication expires; set to -1 if token never expires in this case
   });
--- settings/settings_dev.js
+++ settings/settings_dev.js
@@ -4,9 +4,9 @@
 angular.module('ubirchAdminCrudApp')
   .constant('settings', {
     CONTEXT: "ubirch-admin-ui-dev",
-    UBIRCH_API_HOST : "http://localhost:8080",
-    UBIRCH_AUTH_SERVICE_API_HOST : "http://localhost:8091",
-    UBIRCH_USER_SERVICE_API_HOST: "http://localhost:8091",
+    UBIRCH_API_HOST : "http://ubirch.dev.ubirch.com:80",
     DEFAULT_DEVICE_TYPE_KEY: "unknownDeviceType",
+    UBIRCH_AUTH_SERVICE_API_HOST : "http://auth.dev.ubirch.com:8091",
+    UBIRCH_USER_SERVICE_API_HOST : "http://auth.dev.ubirch.com:8091",
     DEFAULT_AUTH_EXPIRED_SECS: -1   // if OP didn't deliver expires_in parameter this is the default number of seconds after which authentication expires; set to -1 if token never expires in this case
   });
