bb634af0a65de06306910ce61db7c981e37c3bbe
dieAte <beate.fiss@cameo.io>
2017-07-03 12:04:06 +0200

    UBI-244: known bug: registration ends in spinner: no account data accessable -> token get lost

--- app/scripts/controllers/check-activation.js
+++ app/scripts/controllers/check-activation.js
@@ -8,25 +8,38 @@
  * Controller of the ubirchAdminCrudApp
  */
 angular.module('ubirchAdminCrudApp')
-  .controller('CheckActivationCtrl', ['$scope', '$location', 'UserService', function ($scope, $location, UserService) {
+  .controller('CheckActivationCtrl', ['$scope', '$location', '$log', 'UserService', 'constants', '$timeout',
+    function ($scope, $location, $log, UserService, constants, $timeout) {
 
     $scope.workinprogress = true;
     $scope.notactivated = undefined;
 
 
-    UserService.getAccount().$promise.then(
-      function(res){
-        var isActivated = res.activeUser === true ? true : false;
-        UserService.setActivationFlag(isActivated);
-        if (isActivated) {
-          $scope.workinprogress = false;
-          $scope.notactivated = false;
-          $location.url('/');
-        }
-        else {
-          $scope.workinprogress = false;
-          $scope.notactivated = true;
-        }
+    var gotAccountData = function(res){
+      var isActivated = res.activeUser === true ? true : false;
+      UserService.setActivationFlag(isActivated);
+      if (isActivated) {
+        $scope.workinprogress = false;
+        $scope.notactivated = false;
+        $location.url('/');
       }
-    );
+      else {
+        $scope.workinprogress = false;
+        $scope.notactivated = true;
+      }
+    };
+
+    (function reloadUserData() {
+      UserService.getAccount().$promise.then(
+        gotAccountData,
+        function (error) {
+          if (error.data.errorType === "NoUserInfoFound"){
+                $timeout(reloadUserData, constants.POLLING_INTERVAL);
+                $log.warn("No NoUserInfoFound: "+error);
+          }
+          console.log(error);
+        }
+      );
+
+    })();
   }]);
