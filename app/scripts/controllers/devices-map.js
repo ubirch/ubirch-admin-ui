0619d865d7e34419ba6a5cb5bebbce0184181ae5
dieAte <beate.fiss@cameo.io>
2017-08-24 17:13:03 +0200

    devices list with selection of several devices

--- app/scripts/controllers/devices-map.js
+++ app/scripts/controllers/devices-map.js
@@ -8,87 +8,34 @@
  * Controller of the ubirchAdminCrudApp
  */
 angular.module('ubirchAdminCrudApp')
-  .controller('DevicesMapCtrl', [ '$scope', 'Device', function($scope, Device) {
-    var appPath = "http://admin.ubirch.com/#/device-details/";
+  .controller('DevicesMapCtrl', [ '$scope', 'Device', 'deviceTypesList', function($scope, Device, deviceTypesList) {
 
-    $scope.devices = Device.getDevicesList(
-      function(){
-        for (var i=0; i<$scope.devices.length; i++){
 
-          var device = $scope.devices[i];
+    $scope.deviceTypes = deviceTypesList;
 
-          var message = device.deviceType;
-          // message: "Temperature Sensor<br><strong>18,5°C</strong><br><a href='http://admin.ubirch.com/#/device-details/0c5a19bf-194c-40ea-bf46-0416a176aedb'><br>open sensor data</a>!",
-          message += "<br><strong>";
-          switch(device.deviceType){
-            case "lightsSensor":
-              message += "r:" + device.avatarState.reported.r + ", g:" + device.avatarState.reported.g + ", b:" + device.avatarState.reported.b;
-              break;
-            case "temperatureSensor":
-              message += device.avatarState.reported.t + "°C";
-              break;
-            case "waterSensor":
-              message += device.avatarState.reported.w + "%";
-              break;
-            default:
-              message += "...";
-          }
-          message += "</strong>";
-          message += "<br><a href='";
-          message += appPath + device.deviceId;
-          message += "'><br>open sensor data</a>!";
-
-          var marker = {
-            focus: true,
-            draggable: false,
-            lat: parseFloat(device.avatarState.reported.la),
-            lng: parseFloat(device.avatarState.reported.lo),
-            icon: icons[device.deviceType] !== undefined ? icons[device.deviceType] : icons.default,
-            message: message
-          };
-
-          $scope.markers["marker"+i] = marker;
+    (function refreshData() {
+      // Assign to scope within callback to avoid data flickering on screen
+      Device.getDevicesList(function (data) {
+          $scope.devices = data;
         }
-      }
-    );
+      );
+    })();
 
-    angular.extend($scope, {
-      center: {
-        lat: 52.50466320614026,
-        lng: 13.480031490325928,
-        zoom: 12
-      },
-      markers: {},
-      defaults: {
-        scrollWheelZoom: false
-      }
-    });
+    var selectedDevices = {};
 
-    var icons = {
-      default: {
-        type: 'awesomeMarker',
-        prefix: 'ion',
-        icon: 'android-wifi',
-        markerColor: 'gray'
-      },
-      temperatureSensor: {
-        type: 'awesomeMarker',
-        prefix: 'ion',
-        icon: 'thermometer',
-        markerColor: 'red'
-      },
-      lightsSensor: {
-        type: 'awesomeMarker',
-          prefix: 'ion',
-          icon: 'ios-lightbulb',
-          markerColor: 'green'
-      },
-      waterSensor: {
-        type: 'awesomeMarker',
-          prefix: 'ion',
-          icon: 'waterdrop',
-          markerColor: 'blue'
+    $scope.switchDeviceSelection = function(id){
+      if (selectedDevices[id] === undefined){
+        selectedDevices[id] = false;
       }
+      selectedDevices[id] = !selectedDevices[id];
+    };
+
+    $scope.selected = function(id) {
+      return selectedDevices[id] !== undefined && selectedDevices[id];
+    };
+
+    $scope.match = function(key) {
+      return function(collection) { return collection.key.match(key); };
     };
 
   }]);
