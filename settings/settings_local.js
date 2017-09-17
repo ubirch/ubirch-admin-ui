b1d7504a24702d8f4d0a0ff8a350d40e1a0bce3a
derMicha <dermicha@dermicha.de>
2017-09-17 21:56:09 +0200

    merged with ed25119

--- settings/settings_local.js
+++ settings/settings_local.js
@@ -8,11 +8,13 @@ angular.module('ubirchAdminCrudApp')
     UBIRCH_API_HOST : "http://localhost:8080",
     UBIRCH_AUTH_SERVICE_API_HOST : "http://localhost:8091",
     UBIRCH_USER_SERVICE_API_HOST: "http://localhost:8091",
+    UBIRCH_KEY_SERVICE_API_HOST: "http://localhost:8095",
     DEFAULT_DEVICE_TYPE_KEY: "unknownDeviceType",
     DEFAULT_AUTH_EXPIRED_SECS: -1,   // if OP didn't deliver expires_in parameter this is the default number of seconds after which authentication expires; set to -1 if token never expires in this case
     MQTT_SERVER: {
       HOST: "localhost",
       PORT: "1883"
     },
-    ENV_ID: "local-dev"
+    ENV_ID: "local-dev",
+    KEY_GENERATION_FOR_DEVICE_TYPES: ["calliopeMini"]
   });
