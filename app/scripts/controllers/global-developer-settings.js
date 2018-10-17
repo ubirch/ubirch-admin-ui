fd006d3ade83c1892c214391ad39021969d40055
dieAte <beate.fiss@cameo.io>
2018-10-17 23:26:22 +0200

    UP-275, UP-265: global developer infos, contain token and python script example, groups of user missing

--- app/scripts/controllers/global-developer-settings.js
+++ app/scripts/controllers/global-developer-settings.js
@@ -8,10 +8,21 @@
  * Controller of the ubirchAdminCrudApp
  */
 angular.module('ubirchAdminCrudApp')
-  .controller('GlobalDeveloperSettingsCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('GlobalDeveloperSettingsCtrl',['$scope', '$sessionStorage', 'constants',
+    function ($scope, $sessionStorage, constants) {
+
+    $scope.devInfo = {};
+
+    function initInfos() {
+      $scope.devInfo = {
+        avatarServiceDocuUrl: constants.AVATAR_SERVICE_DOCUMENTATION,
+        ubirchProtocolDocuUrl: constants.UBIRCH_PROTOCOL_DOCUMENTATION,
+        token: $sessionStorage.token.token,
+        tokenPraefix: constants.TOKEN_PRAEFIX,
+        tokenSuffix: constants.TOKEN_SUFFIX
+      };
+    }
+
+    initInfos();
+
+  }]);
