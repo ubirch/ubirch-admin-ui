924dbf95a64e76fdb4b9bda8719a6de0f60e2fb1
Matthias L. Jugel <leo@thinkberg.com>
2018-10-19 16:09:06 +0200

    fix bower_components not existing

--- buildWeb.sh
+++ buildWeb.sh
@@ -2,6 +2,7 @@
 env
 echo build ubirch admin UI
 echo "create dist version for stage >>$1<<"
+mkdir bower_components
 echo install missing npm components
 npm install
 echo install missing bower components
